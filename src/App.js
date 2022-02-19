91d2f9101ae7a9d5a538e4d3ada1c28053cbb7d3
MohamedMesrah <mohamed.mesrah@gmail.com>
2022-02-19 21:31:25 +0200

    show home to registered users only

--- src/App.js
+++ src/App.js
@@ -1,18 +1,17 @@
-// import { useContext } from "react";
+import { useContext } from "react";
 import { Routes, Route } from "react-router-dom";
 import Register from "./components/register/register";
 import Home from "./components/home/home";
 
-// import { AppContext } from "./context/appContext";
+import { AppContext } from "./context/appContext";
 
 function App() {
-  // const { registered } = useContext(AppContext);
+  const { registered } = useContext(AppContext);
 
   return (
     <div className="App">
       <Routes>
-        <Route path="/home" element={<Home />}></Route>
-        {/* {registered && <Route path="/home" element={<Home />} />} */}
+        {registered && <Route path="/home" element={<Home />} />}
         <Route path="/" element={<Register />} />
         <Route path="*" element={<h1>Not Found!</h1>} />
       </Routes>
